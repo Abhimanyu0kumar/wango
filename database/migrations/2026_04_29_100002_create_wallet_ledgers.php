e028d24ac7c1107d644219e3fccac20d824fd4fe
abhimanyu0kumar <abhimanyukumar6500ssn@gmail.com>
2026-05-05 10:46:23 +0530

    correct table name for migration

--- database/migrations/2026_04_29_100002_create_wallet_ledgers.php
+++ database/migrations/2026_04_29_100002_create_wallet_ledgers.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('wallet_ledger', function (Blueprint $table) {
+        Schema::create('wallet_ledgers', function (Blueprint $table) {
             $table->id();
 
             $table->foreignId('wallet_id')
@@ -67,6 +67,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('wallet_ledger');
+        Schema::dropIfExists('wallet_ledgers');
     }
 };
